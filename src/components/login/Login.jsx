baacf923491565a6d9ed6272eff90a6d03f63bdf
Sevgy <121260146+Sevgy@users.noreply.github.com>
2023-12-15 01:12:28 +0200

    Added Login form

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,4 +1,6 @@
 import styles from './Login.module.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default function Login() {
     return (
@@ -6,10 +8,24 @@ export default function Login() {
             <div className={styles['login-container']}>
                 <div className={styles['login-form-container']}>
                     <h2>Login to Your Account</h2>
-                    {/* Add your login form here */}
-                    <form>
-                        {/* Your login form fields go here */}
-                    </form>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
                 </div>
             </div>
         </div>
