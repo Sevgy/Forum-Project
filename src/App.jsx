ed6a4c58808eb7eae9327aaa9c71c7e8ed0e23c8
Sevgy <121260146+Sevgy@users.noreply.github.com>
2023-12-14 23:48:37 +0200

    implemented Routing
    Better structuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import Footer from "./components/Footer.jsx"
-import Header from "./components/Header.jsx"
+import { Route, Routes } from "react-router-dom"
 
+
+import Footer from "./components/footer/Footer.jsx"
+import Header from "./components/header/Header.jsx"
 
 function App() {
   return (
     <>
       <Header/>
+      <Routes>
+        <Route></Route>
+      </Routes>
 
       <Footer/>
     </>
