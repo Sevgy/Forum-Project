aa6128ccd81daed4fa8577ad90a9a5a6a5e9b4ed
Sevgy <121260146+Sevgy@users.noreply.github.com>
2023-12-14 23:58:09 +0200

    Updated the Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <>
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">BicepsForum</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link as={Link} to="/posts">Posts</Nav.Link>
+                        <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                        <Nav.Link as={Link} to="/register">Register</Nav.Link>
+                        <Nav.Link as={Link} to="/logout">Log Out</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
