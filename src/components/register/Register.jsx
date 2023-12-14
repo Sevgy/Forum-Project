af9169ea7e8b6a59e809c9c596093e393559b4d1
Sevgy <121260146+Sevgy@users.noreply.github.com>
2023-12-15 01:16:35 +0200

    Added Register form

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,17 +1,39 @@
 import styles from './Register.module.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 export default function Register() {
     return (
         <div className={styles['register-background']}>
-          <div className={styles['register-container']}>
-            <div className={styles['register-form-container']}>
-              <h2>Create an Account</h2>
-              {/* Add your registration form here */}
-              <form>
-                {/* Your registration form fields go here */}
-              </form>
+            <div className={styles['register-container']}>
+                <div className={styles['register-form-container']}>
+                    <h2>Create an Account</h2>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
             </div>
-          </div>
         </div>
-      );
+    );
 }
