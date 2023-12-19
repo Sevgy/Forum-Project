bcf0410ecb058466bcdfb37c260fb710135ad841
Sevgy <121260146+Sevgy@users.noreply.github.com>
2023-12-19 12:06:40 +0200

    Update PostListItem.jsx

--- src/components/post-list/post-list-item/PostListItem.jsx
+++ src/components/post-list/post-list-item/PostListItem.jsx
@@ -7,11 +7,17 @@ export default function PostListItem({
     text,
     email,
 }) {
+    function truncateText(text, limit) {
+        return text.length > limit ? `${text.slice(0, limit)}...` : text;
+    }
+
+    const truncatedText = truncateText(text, 150);
+
     return (
         <div className={styles['post-list-item']}>
             <Link to={`/posts/${_id}`} className={styles['post-link']}>
                 <h3 className={styles['post-title']}>{title}</h3>
-                <p className={styles['post-content']}>{text}</p>
+                <p className={styles['post-content']}>{truncatedText}</p>
                 <div className={styles['post-meta']}>
                     <span>Author: {email}</span>
                     {/* <span>Posted on: Not determined yet</span> */}
